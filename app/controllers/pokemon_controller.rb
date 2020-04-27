62844ed46ab695b4990d3068311bed1187371076
jmingrove21 <jmingrove21@gmail.com>
2020-04-27 13:30:44 +0900

    pokemon project fin

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,2 +1,19 @@
 class PokemonController < ApplicationController
+
+    def index
+        @poke=Pokemon.find_by(id:1)
+    end
+    def volt
+        @poke=Pokemon.find_by(id:1)
+        @poke.hp-=2
+        @poke.save
+    end
+    def speed
+        @hp_value=params[:hp]
+    
+        @poke=Pokemon.find_by(id:1)
+        @poke.hp-=@hp_value.to_i()
+        @poke.save
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "home#index"
-  
-
+  get "/pokemon" => "pokemon#index"
+  get "/pokemon/volt" => "pokemon#volt"
+  post "/pokemon/speed" => "pokemon#speed"
 end
