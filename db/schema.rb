a12c65ad092549c81f697a68eece0a307fc96651
jmingrove21 <jmingrove21@gmail.com>
2020-05-08 15:27:18 +0900

    restore user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_055523) do
+ActiveRecord::Schema.define(version: 2020_05_08_054400) do
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
